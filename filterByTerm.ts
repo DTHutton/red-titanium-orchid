0c056b4288b73f5a994ae193d9eba444dfc0dc38
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-12-23 21:48:29 -0500

    inital commit and first lesson finished

---
+++ filterByTerm.ts
@@ -0,0 +1,15 @@
+interface Link {
+    url: string;
+}
+
+function filterByTerm(input: Array<Link>, searchTerm: string) {
+    if (!searchTerm) throw Error("searchTerm cannot be empty");
+    if (!input.length) throw Error("input cannot be empty");
+    const regex = new RegExp(searchTerm, "i");
+    return input.filter(function (arrayElement) {
+        return arrayElement.url.match(regex);
+    });
+}
+filterByTerm(
+    [{ url: "string1" }, { url: "string2" }, { url: "string3" }],
+    "java");
