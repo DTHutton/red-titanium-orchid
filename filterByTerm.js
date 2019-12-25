02bc1b28c8a30f1ae18ab6fef63daa63cf03729a
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-12-24 23:56:50 -0500

    finshed up to TypeScript tutorial for beginners: type aliases vs interfaces

--- filterByTerm.js
+++ filterByTerm.js
@@ -1,17 +1,42 @@
 "use strict";
-function filterByTerm(input, searchTerm) {
+;
+;
+const link1 = {
+    description: "Typescript tutorial for beginners is a tutorial for all the JavaSCript developers ...",
+    id: 1,
+    url: "www.valentinog.com/typsecript/",
+    language: "en"
+};
+function filterByTerm(input, searchTerm, lookupKey = "url") {
     if (!searchTerm)
         throw Error("searchTerm cannot be empty");
     if (!input.length)
         throw Error("input cannot be empty");
     const regex = new RegExp(searchTerm, "i");
     return input.filter(function (arrayElement) {
-        return arrayElement.url.match(regex);
-    });
+        return arrayElement[lookupKey].match(regex);
+    }).toString();
 }
+;
 const obj1 = { url: "string1" };
 const obj2 = { url: "string2" };
 const obj3 = { url: "string3" };
 const arrOfLinks = [obj1, obj2, obj3];
 const term = "java";
 filterByTerm(arrOfLinks, term);
+// const paolo = {
+//     name: "Paolo",
+//     city: "Siena",
+//     age: 44
+// };
+// const tom = {
+//     name: "Tom",
+//     city: "Munich",
+//     age: 33
+// };
+// function filterPerson(arr, term, key) {
+//     return arr.filter(function (person) {
+//         return person[key].match(term);
+//     });
+// };
+// filterPerson([paolo, tom], "Siena", "city");
--- filterByTerm.ts
+++ filterByTerm.ts
@@ -2,16 +2,28 @@ interface Link {
     description?: string;
     id?: number;
     url: string;
-}
+    [index: string]: any;
+};
 
-function filterByTerm(input: Array<Link>, searchTerm: string) {
+interface TransaltedLink extends Link {
+    language: string;
+};
+
+const link1: TransaltedLink = {
+    description: "Typescript tutorial for beginners is a tutorial for all the JavaSCript developers ...",
+    id: 1,
+    url: "www.valentinog.com/typsecript/",
+    language: "en"
+};
+
+function filterByTerm(input: Array<Link>, searchTerm: string, lookupKey: string = "url"): Array<Link> {
     if (!searchTerm) throw Error("searchTerm cannot be empty");
     if (!input.length) throw Error("input cannot be empty");
     const regex = new RegExp(searchTerm, "i");
     return input.filter(function (arrayElement) {
-        return arrayElement.url.match(regex);
+        return arrayElement[lookupKey].match(regex);
     });
-}
+};
 
 const obj1: Link = { url: "string1" };
 const obj2: Link = { url: "string2" };
@@ -21,3 +33,23 @@ const arrOfLinks: Array<Link> = [obj1, obj2, obj3];
 const term: string = "java";
 
 filterByTerm(arrOfLinks, term);
+
+// const paolo = {
+//     name: "Paolo",
+//     city: "Siena",
+//     age: 44
+// };
+
+// const tom = {
+//     name: "Tom",
+//     city: "Munich",
+//     age: 33
+// };
+
+// function filterPerson(arr, term, key) {
+//     return arr.filter(function (person) {
+//         return person[key].match(term);
+//     });
+// };
+
+// filterPerson([paolo, tom], "Siena", "city");
